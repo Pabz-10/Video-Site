8eaa628a3a626f1df048946783ff8c778ea6344c
Pabil <pabil.adhikari10@gmail.com>
2023-10-10 17:39:24 -0700

    Setup sign in and firebase SDK with Auth

--- video-web-client/app/navbar/navbar.tsx
+++ video-web-client/app/navbar/navbar.tsx
@@ -1,8 +1,23 @@
+"use client";
 import Image from "next/image";
 import Link from "next/link";
 import styles from "./navbar.module.css";
+import SignIn from "./signin";
+import { onAuthStateChangedHelper } from "../firebase/firebase";
+import { useState, useEffect } from "react";
+import { User } from "firebase/auth";
 
 export default function Navbar() {
+	const [user, setUser] = useState<User | null>(null);
+
+	// listens for authentication changes
+	useEffect(() => {
+		const unsubscribe = onAuthStateChangedHelper((user) => {
+			setUser(user);
+		});
+
+		return () => unsubscribe();
+	});
 	return (
 		<nav className={styles.nav}>
 			<Link href="/">
@@ -13,6 +28,7 @@ export default function Navbar() {
 					alt="Youtube logo"
 				/>
 			</Link>
+			<SignIn user={user} />
 		</nav>
 	);
 }

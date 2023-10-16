02d814159d08e4972f8ee81cfee2a9ffddb7ef79
Pabil <pabil.adhikari10@gmail.com>
2023-10-16 00:59:07 -0700

    Added upload video feature to the web app

--- video-web-client/app/navbar/navbar.tsx
+++ video-web-client/app/navbar/navbar.tsx
@@ -6,6 +6,7 @@ import SignIn from "./signin";
 import { onAuthStateChangedHelper } from "../firebase/firebase";
 import { useState, useEffect } from "react";
 import { User } from "firebase/auth";
+import Upload from "./upload";
 
 export default function Navbar() {
 	const [user, setUser] = useState<User | null>(null);
@@ -25,9 +26,10 @@ export default function Navbar() {
 					width={90}
 					height={20}
 					src="/youtube-logo.svg"
-					alt="Youtube logo"
+					alt="YouTube Logo"
 				/>
 			</Link>
+			{user && <Upload />}
 			<SignIn user={user} />
 		</nav>
 	);

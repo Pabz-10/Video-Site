bfdb7cb80d7484e3a42a2c0b5b9d19c670941997
Pabil <pabil.adhikari10@gmail.com>
2023-10-16 20:13:00 -0700

    Aded get vids function

--- api-service/functions/src/index.ts
+++ api-service/functions/src/index.ts
@@ -2,6 +2,7 @@ import * as functions from "firebase-functions";
 import { initializeApp } from "firebase-admin/app";
 import { Firestore } from "firebase-admin/firestore";
 import * as logger from "firebase-functions/logger";
+
 import { Storage } from "@google-cloud/storage";
 import { onCall } from "firebase-functions/v2/https";
 
@@ -9,8 +10,18 @@ initializeApp();
 
 const firestore = new Firestore();
 const storage = new Storage();
-
 const rawVideoBucketName = "pab-raw-videos";
+
+const videoCollectionId = "videos";
+
+export interface Video {
+	id?: string;
+	uid?: string;
+	filename?: string;
+	status?: "processing" | "processed";
+	title?: string;
+	description?: string;
+}
 
 export const createUser = functions.auth.user().onCreate((user) => {
 	const userInfo = {
@@ -52,3 +63,11 @@ export const generateUploadUrl = onCall(
 		return { url, fileName };
 	}
 );
+
+export const getVideos = onCall({ maxInstances: 1 }, async () => {
+	const querySnapshot = await firestore
+		.collection(videoCollectionId)
+		.limit(10)
+		.get();
+	return querySnapshot.docs.map((doc) => doc.data());
+});
